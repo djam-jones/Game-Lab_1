0fd17365028c1e96e0fb0d22dadc0e0340a4e352
kevinboogaard <kevin.boogaard3@gmail.com>
2015-04-21 16:08:35 +0200

    Kevin 003
    Room

---
+++ New Unity Project/Assets/Scripts/Tags.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tags : MonoBehaviour {
+
+    public const string buttonTag = "Button";
+    public const string handTag = "Hand";
+    public const string plantTag = "Plant";
+
+
+}
