8682f698763e857e351773948cd565abb21e1f1c
kevinboogaard <kevin.boogaard3@gmail.com>
2015-04-21 17:03:36 +0200

    kevin 004
    events

---
+++ New Unity Project/Assets/Scripts/Events.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Events : MonoBehaviour {
+
+    private static Events _instance;
+
+    public static Events Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType(typeof(Events)) as Events;
+            }
+
+            return _instance;
+        }
+    }
+	
+    public void PlantEvent()
+    {
+        GameObject.FindGameObjectWithTag(Tags.plantTag).transform.position = new Vector3(-2, 0, 8.5f);
+    }
+
+    public void CrocoEvent()
+    {
+        GameObject.FindGameObjectWithTag(Tags.crocoTag).transform.position = new Vector3(this.transform.position.x, this.transform.position.y + 1, this.transform.position.z);
+    }
+}
