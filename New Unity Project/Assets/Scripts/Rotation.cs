57fc3cd72ecefdbb30acc93c3027aa3c9fdc3ebd
kevinboogaard <kevin.boogaard3@gmail.com>
2015-04-23 10:29:21 +0200

    kevin 006
    space

---
+++ New Unity Project/Assets/Scripts/Rotation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Rotate(Vector3.right * Time.deltaTime * 10);
+        this.transform.Rotate(Vector3.up * Time.deltaTime * 2);
+    }
+}
